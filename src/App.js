f6d694b03504dbd5bf92f57ab1e520d67b11e773
Joy <joy50422@gmail.com>
2023-11-12 14:52:32 +0800

    init react + chakra + styled-components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Heading } from '@chakra-ui/react'
+
+function App() {
+  return (
+    <div className="App">
+        <Heading as='h2' size='xl'>
+            Hello
+        </Heading>
+    </div>
+  );
+}
+
+export default App;
